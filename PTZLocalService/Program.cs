691024db6d0b63ad9494cd90a04868c3d712a19c
Wuhao Ouyang <strider.oy@gmail.com>
2017-08-23 10:49:44 -0700

    Remove redundant

--- PTZLocalService/Program.cs
+++ PTZLocalService/Program.cs
@@ -1,11 +1,4 @@
 ﻿using System;
-using System.Text;
-using WebSocketSharp;
-using WebSocketSharp.Net;
-using WebSocketSharp.Server;
-using PTZ;
-using System.ServiceProcess;
-using System.Text.RegularExpressions;
 
 namespace PTZLocalService
 {
@@ -17,50 +10,6 @@ namespace PTZLocalService
         public static void Main()
         {
             System.ServiceProcess.ServiceBase.Run(new LocalService());
-
-            //PTZDevice device;
-            //HttpServer httpsv = new HttpServer(4649);
-
-            //httpsv.OnGet += (sender, e) =>
-            //{
-            //    HttpListenerRequest req = e.Request;
-            //    HttpListenerResponse res = e.Response;
-
-            //    String msg = "";
-            //    if (req.QueryString.ToString().Contains("init"))
-            //    {
-            //        try
-            //        {
-            //            device = PTZDevice.GetDevice("BCC950 ConferenceCam", PTZType.Relative);
-            //        }
-            //        catch (Exception ex)
-            //        {
-            //            msg = ex.Message;
-            //        }
-            //    }
-
-            //    String json = "{\"result\":\"" + msg + "\", \"query\":\"" + req.QueryString + "\"}";
-            //    res.StatusCode = (int)HttpStatusCode.OK;
-            //    res.ContentType = "application/json";
-            //    res.ContentEncoding = Encoding.UTF8;
-
-            //    res.WriteContent(Encoding.UTF8.GetBytes(json));
-            //};
-
-            //httpsv.Start();
-            //if (httpsv.IsListening)
-            //{
-            //    Console.WriteLine("Listening on port {0}, and providing WebSocket services:", httpsv.Port);
-            //    foreach (var path in httpsv.WebSocketServices.Paths)
-            //        Console.WriteLine("- {0}", path);
-            //}
-
-            //Console.WriteLine("\nPress Enter key to stop the server...");
-            //Console.ReadLine();
-
-            //while (true) ;
-
-            //httpsv.Stop();
         }
     }
 }
